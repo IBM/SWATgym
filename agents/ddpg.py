c650f339f99129278a120c256ee9e7e1fdc8ab85
Malvern <madondomalvern@gmail.com>
2023-02-27 13:22:16 -0500

    :shipit: adding RL agents
    Signed-off-by: Malvern <madondomalvern@gmail.com>

---
+++ agents/ddpg.py
@@ -0,0 +1,155 @@
+"""
+Implementation of Deep Deterministic Policy Gradients (DDPG)
+    Paper: https://arxiv.org/abs/1509.02971
+    Code: https://github.com/sfujim/BCQ/blob/master/continuous_BCQ/DDPG.py
+"""
+
+import copy
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+# ddpg hyperparameters
+lr_mu        = 0.001
+lr_q         = 0.01
+gamma        = 0.99
+batch_size   = 16
+buffer_limit = 1000
+tau          = 0.005 
+
+
+class ReplayBuffer(object):
+    def __init__(self, state_dim, action_dim, device, max_size=int(buffer_limit)):
+        self.max_size = max_size
+        self.ptr = 0
+        self.size = 0
+
+        self.state = np.zeros((max_size, state_dim))
+        self.action = np.zeros((max_size, action_dim))
+        self.next_state = np.zeros((max_size, state_dim))
+        self.reward = np.zeros((max_size, 1))
+        self.not_done = np.zeros((max_size, 1))
+        self.device = device
+
+    def add(self, state, action, next_state, reward, done):
+        self.state[self.ptr] = state
+        self.action[self.ptr] = action
+        self.next_state[self.ptr] = next_state
+        self.reward[self.ptr] = reward
+        self.not_done[self.ptr] = 1. - done
+
+        self.ptr = (self.ptr + 1) % self.max_size
+        self.size = min(self.size + 1, self.max_size)
+
+    def sample(self, batch_size):
+        ind = np.random.randint(0, self.size, size=batch_size)
+
+        return (
+        torch.FloatTensor(self.state[ind]).to(self.device),
+        torch.FloatTensor(self.action[ind]).to(self.device),
+        torch.FloatTensor(self.next_state[ind]).to(self.device),
+        torch.FloatTensor(self.reward[ind]).to(self.device),
+        torch.FloatTensor(self.not_done[ind]).to(self.device)
+        )
+
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, max_action):
+        super(Actor, self).__init__()
+        self.fc1 = nn.Linear(state_dim, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc_mu = nn.Linear(64, action_dim)
+        self.max_action = max_action
+
+    def forward(self, state):
+        a = F.relu(self.fc1(state))
+        a = F.relu(self.fc2(a))
+        mu = self.max_action*torch.tanh(self.fc_mu(a))
+        return mu
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Critic, self).__init__()
+        self.l1 = nn.Linear(state_dim + action_dim, 64)
+        self.l2 = nn.Linear(64, 32)
+        self.l3 = nn.Linear(32, 1)
+
+    def forward(self, state, action):
+        q = F.relu(self.l1(torch.cat([state, action], 1)))
+        q = F.relu(self.l2(q))
+        return self.l3(q)
+
+
+class DDPG(object):
+    def __init__(self, state_dim, action_dim, max_action, device, discount=0.99, tau=0.005):
+        self.actor = Actor(state_dim, action_dim, max_action).to(device)
+        self.actor_target = copy.deepcopy(self.actor)
+        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=1e-4)
+
+        self.critic = Critic(state_dim, action_dim).to(device)
+        self.critic_target = copy.deepcopy(self.critic)
+        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=1e-3)
+
+        self.discount = discount
+        self.tau = tau
+        self.device = device
+
+    def select_action(self, state): 
+        state = torch.FloatTensor(state.reshape(1, -1)).to(self.device)
+        return self.actor(state).cpu().data.numpy().flatten()
+
+    def train(self, replay_buffer, batch_size=100):
+        # Sample replay buffer 
+        state, action, next_state, reward, not_done = replay_buffer.sample(batch_size)
+
+        # Compute the target Q value
+        target_Q = self.critic_target(next_state, self.actor_target(next_state))
+        target_Q = reward + (not_done * self.discount * target_Q).detach()
+
+        # Get current Q estimate
+        current_Q = self.critic(state, action)
+
+        # Compute critic loss: smooth_l1_loss or mse_loss
+        critic_loss = F.smooth_l1_loss(current_Q, target_Q)
+
+        # Optimize the critic
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        # Compute actor loss
+        actor_loss = -self.critic(state, self.actor(state)).mean()
+
+        # Optimize the actor 
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        # Update the frozen target models
+        for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
+            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+        for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
+            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+        return actor_loss.item(), critic_loss.item()
+      
+# Runs policy for X episodes and returns average reward
+def eval_policy(policy, max_action, eval_episodes=10):
+    eval_env = SWATEnv()
+    avg_reward = 0.
+
+    for t in range(eval_episodes):
+        state, _, done, info = eval_env.reset()
+        while not done:
+            action = policy.select_action(np.array(state)).clip(0, max_action)
+            state, reward, done, _ = eval_env.step(action)
+            avg_reward += reward
+
+    avg_reward /= eval_episodes
+    print("---------------------------------------")
+    print(f"Evaluation over {eval_episodes} episodes: {avg_reward:.3f}")
+    print("---------------------------------------")
+    return avg_reward
